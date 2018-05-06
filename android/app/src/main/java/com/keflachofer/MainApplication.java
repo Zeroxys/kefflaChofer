4784399131c706769215ce7d7467e68c0b039dbe
zeroxys <miguelzavalac@gmail.com>
2018-05-05 23:35:41 -0500

    adding facebooksdk

--- android/app/src/main/java/com/keflachofer/MainApplication.java
+++ android/app/src/main/java/com/keflachofer/MainApplication.java
@@ -9,6 +9,7 @@ import android.app.Application;
 import com.oblador.vectoricons.VectorIconsPackage;
 
 import com.facebook.react.ReactApplication;
+import com.facebook.reactnative.androidsdk.FBSDKPackage;
 import com.facebook.react.ReactNativeHost;
 import com.facebook.react.ReactPackage;
 import com.facebook.react.shell.MainReactPackage;
@@ -17,7 +18,18 @@ import com.facebook.soloader.SoLoader;
 import java.util.Arrays;
 import java.util.List;
 
+import com.facebook.CallbackManager;
+import com.facebook.FacebookSdk;
+import com.facebook.reactnative.androidsdk.FBSDKPackage;
+import com.facebook.appevents.AppEventsLogger;
+
 public class MainApplication extends Application implements ReactApplication {
+
+  private static CallbackManager mCallbackManager = CallbackManager.Factory.create();
+
+  protected static CallbackManager getCallbackManager() {
+    return mCallbackManager;
+  }
 
   private final ReactNativeHost mReactNativeHost = new ReactNativeHost(this) {
     @Override
@@ -31,8 +43,9 @@ public class MainApplication extends Application implements ReactApplication {
           new MainReactPackage(),
           new LinearGradientPackage(),
           new SplashScreenReactPackage(),
-              new MapsPackage(),
-          new VectorIconsPackage()
+          new MapsPackage(),
+          new VectorIconsPackage(),
+          new FBSDKPackage(mCallbackManager)
       );
     }
 
